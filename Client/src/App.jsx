e9eef6ebb6d66b62d7a1a2415c7459e32072a1ed
AlexJaegerPena <166617483+AlexJaegerPena@users.noreply.github.com>
2024-06-28 16:03:00 +0200

    First commit Poke Card

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import PokeCard from "./components/PokeCard";
+import PokeCardFight from "./components/PokeCardFight";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="App">
         <h1>Hello World</h1>
+        <Header />
+        <PokeCard />
+        <PokeCardFight />
+        <Footer />
       </div>
     </>
   );
