a7de73d960a53a9fc53cad0eb92c52ec85d6b97c
mdMehediBillah <mailbillah@gmail.com>
2024-07-02 15:08:12 +0200

    client site connedted with mongoose

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,25 +1,7 @@
-
-import React from "react";
-import "./App.css";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Signup />} />
-      </Routes>
-    </Router>
-    // <>
-    //   <div className="App">
-    //     <Signup />
-    //   </div>
-    // </>
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Components
@@ -30,23 +12,39 @@ import Gamepage from "./pages/GamePage/Gamepage";
 import DetailPage from "./pages/DetailPage/DetailPage";
 import AllPoke from "./pages/AllPoke/AllPoke";
 import ScorePage from "./pages/ScorePage/ScorePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
+// import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          {/* <Route path="/" element={<LoginPage />} /> */}
+          <Route path="/" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/allpokes" element={<AllPoke />} />
           <Route path="/allpokes/:id" element={<DetailPage />} />
           <Route path="/gameplay" element={<Gamepage />} />
           <Route path="/score" element={<ScorePage />} />
         </Routes>
+
+        <ToastContainer
+          position="top-right"
+          limit={1}
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </BrowserRouter>
     </>
-
   );
 }
 
