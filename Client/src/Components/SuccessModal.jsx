e6b30d0bec569693dbe0d38f9831fa12e3dc6009
mdMehediBillah <mailbillah@gmail.com>
2024-07-06 22:58:39 +0200

    modals are add for success and lose in Game play page

--- Client/src/Components/SuccessModal.jsx
+++ Client/src/Components/SuccessModal.jsx
@@ -1,11 +1,17 @@
 import "./SuccessModal.css";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 const SuccessModal = ({ closeModal, selectedPokemon }) => {
+  const navigate = useNavigate();
   const userName = localStorage.getItem("userName");
 
   const handleCloseModal = () => {
-    closeModal(false);
+    closeModal(null);
+  };
+  const handlePlayAgain = () => {
+    closeModal(null);
+    navigate("/home");
   };
 
   return (
@@ -35,7 +41,10 @@ const SuccessModal = ({ closeModal, selectedPokemon }) => {
           </p>
         </div>
         <div className="flex mt-6 justify-between gap-4 mt-16">
-          <button className=" bg-orange-300 hover:bg-orange-200 text-black font-semibold py-2 rounded w-full">
+          <button
+            className=" bg-orange-300 hover:bg-orange-200 text-black font-semibold py-2 rounded w-full"
+            onClick={handlePlayAgain}
+          >
             Play again
           </button>
           <button className=" bg-cyan-400 hover:bg-cyan-200 text-black font-semibold py-2 rounded w-full">
--- Client/src/pages/GamePage/Gamepage.jsx
+++ Client/src/pages/GamePage/Gamepage.jsx
@@ -1,31 +1,26 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-
 import imgUrl from "../../images/gamePlayBg.jpg";
-
+import electric from "../../assets/images/pokecard/electric.svg";
+import PokeFight from "../../assets/pokeFight.png";
 import { useEffect, useState } from "react";
-//import { useLocation } from "react-router-dom";
 import axios from "axios";
+import { motion } from "framer-motion";
+import vs from "../../assets/vs.svg";
+import { fetchRandomPokemon } from "../../utils/randomPokeUtils";
 import SuccessModal from "../../Components/SuccessModal";
-import LoseModal from "../../Components/LoseModal.jsx";
+import LoseModal from "../../Components/LoseModal";
+import { toast } from "react-toastify";
 
 const Gamepage = () => {
-  const [openModal, setOpenModal] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
-  const selectedPokemon = location.state?.selectedPokemon;
   const [randomPokemon, setRandomPokemon] = useState(null);
   const [winner, setWinner] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  // const [openModal, setOpenModal] = useState(false);
+  const [showModal, setShowModal] = useState(null);
 
-  const handlePlay = () => {
-    // setTimeout(() => {setOpenModal(false);}, 3000);
-    // setOpenModal(false);
-    // const playerChoice = Math.floor(Math.random() * 3) + 1;
-    // const randomChoice = Math.floor(Math.random() * 3) + 1;
-    // const result = playerChoice === randomChoice? "It's a tie!" : playerChoice > randomChoice? "Player wins!" : "Computer wins!";
-    // setWinner(result);
-    // setOpenModal(true);
-    // navigate("/result", { state: { selectedPokemon, randomPokemon } });
-  };
+  const selectedPokemon = location.state?.selectedPokemon;
 
   useEffect(() => {
     if (!selectedPokemon) {
@@ -33,178 +28,248 @@ const Gamepage = () => {
       return;
     }
 
-    const fetchRandomPokemon = async () => {
-      const randomPokemonId = Math.floor(Math.random() * 898) + 1;
-      const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`
-      );
-      const pokemonData = response.data;
-
-      const speciesResponse = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon-species/${randomPokemonId}`
-      );
-      const speciesData = speciesResponse.data;
-      const description = speciesData.flavor_text_entries.find(
-        (entry) => entry.language.name === "en"
-      ).flavor_text;
-
-      const formattedPokemon = {
-        id: pokemonData.id,
-        name: pokemonData.name,
-        order: pokemonData.order,
-        stats: {
-          hp: pokemonData.stats.find((stat) => stat.stat.name === "hp")
-            .base_stat,
-          attack: pokemonData.stats.find((stat) => stat.stat.name === "attack")
-            .base_stat,
-          defense: pokemonData.stats.find(
-            (stat) => stat.stat.name === "defense"
-          ).base_stat,
-        },
-        abilities: pokemonData.abilities,
-        height: pokemonData.height,
-        weight: pokemonData.weight,
-        species: pokemonData.species.url,
-        image: pokemonData.sprites.other["official-artwork"].front_default,
-        types: pokemonData.types,
-        description: description,
-      };
-
-      setRandomPokemon(formattedPokemon);
+    const fetchData = async () => {
+      const pokemon = await fetchRandomPokemon();
+      setRandomPokemon(pokemon);
     };
-
-    fetchRandomPokemon();
+    fetchData();
   }, [selectedPokemon, navigate]);
-  // ===
+
+  // Determine winner
+  const determineWinner = () => {
+    const selectedTotal =
+      selectedPokemon.stats.hp +
+      selectedPokemon.stats.attack +
+      selectedPokemon.stats.defense;
+    const randomTotal =
+      randomPokemon.stats.hp +
+      randomPokemon.stats.attack +
+      randomPokemon.stats.defense;
+
+    if (selectedTotal > randomTotal) {
+      setWinner(selectedPokemon.name);
+    } else if (selectedTotal < randomTotal) {
+      setWinner(randomPokemon.name);
+    } else {
+      setWinner("It's a tie!");
+    }
+  };
+  // Show result
   useEffect(() => {
-    if (selectedPokemon && randomPokemon) {
-      const determineWinner = () => {
-        const selectedTotal =
-          selectedPokemon.stats.hp +
-          selectedPokemon.stats.attack +
-          selectedPokemon.stats.defense;
-        const randomTotal =
-          randomPokemon.stats.hp +
-          randomPokemon.stats.attack +
-          randomPokemon.stats.defense;
-
-        if (selectedTotal > randomTotal) {
-          setWinner(selectedPokemon.name);
-        } else if (selectedTotal < randomTotal) {
-          setWinner(randomPokemon.name);
-        } else {
-          setWinner("It's a tie!");
-        }
-      };
-
+    if (selectedPokemon && randomPokemon && showResult) {
       determineWinner();
     }
-  }, [selectedPokemon, randomPokemon]);
+  }, [selectedPokemon, randomPokemon, showResult]);
 
-  if (!selectedPokemon) {
-    return null;
-  }
+  // Save result
+  const saveResult = () => {
+    const fightResult = {
+      selectedPokemon: selectedPokemon.name,
+      randomPokemon: randomPokemon.name,
+      winner: winner,
+      date: new Date().toLocaleString(),
+    };
+    // Check existing results in local storage
+    const existingResults =
+      JSON.parse(localStorage.getItem("fightResults")) || [];
+    existingResults.push(fightResult);
+    localStorage.setItem("fightResults", JSON.stringify(existingResults));
+    toast.success("Fight result saved successfully!");
+    // alert("Fight result saved!");
+  };
 
   if (!randomPokemon) {
     return <div>Loading...</div>;
   }
 
+  // const handleResult = () => {
+  //   setShowResult(true);
+  //   setOpenModal(true);
+  // };
+  // Modal dialog experiment
+
+  const winnerPokemon = winner;
+  const userSelectPokemon = selectedPokemon.name;
+  console.log(winnerPokemon);
+  console.log(userSelectPokemon);
+
+  const handleCheckWinner = () => {
+    if (winnerPokemon === userSelectPokemon) {
+      setShowModal("success");
+      setShowResult(true);
+    } else {
+      setShowModal("fail");
+      setShowResult(true);
+    }
+  };
+
   return (
-    <>
-      {/* {openModal ? (
+    <div
+      className=" homeBg min-h-screen bg-cover bg-center bg-no-repeat w-[100%]"
+      style={{
+        backgroundImage: `url(${imgUrl})`,
+      }}
+    >
+      {/* modal */}
+      {showModal === "success" && (
         <SuccessModal
-          closeModal={setOpenModal}
+          closeModal={setShowModal}
           selectedPokemon={selectedPokemon}
         />
-      ) : (
+      )}
+      {showModal === "fail" && (
         <LoseModal
-          closeModal={setOpenModal}
+          closeModal={setShowModal}
           selectedPokemon={selectedPokemon}
         />
-      )} */}
+      )}
 
-      <div
-        className=" homeBg min-h-screen bg-cover bg-center bg-no-repeat w-[100%]"
-        style={{
-          backgroundImage: `url(${imgUrl})`,
-        }}
-      >
-        <h1 className="text-center bg-cyan-50 pt-8 text-4xl font-semibold">
-          {" "}
-          Fight with Pokemon
-        </h1>
-        <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
-          <Link to="/home">
-            <p>Change</p>
-          </Link>
-          <p>Fight</p>
-        </div>
-        <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
-          <Link to="/score">
-            <p>Scores</p>
-          </Link>
-        </div>
+      {/* {winnerPokemon === userSelectPokemon ? <SuccessModal /> : <LoseModal />} */}
+      <h1 className="text-center bg-cyan-50 pt-8 text-4xl font-semibold">
+        {" "}
+        Fight with Pokemon
+      </h1>
+      <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
+        <Link to="/home">
+          <p>Change Pokemon</p>
+        </Link>
+        {/* <p>Fight</p> */}
+      </div>
+      <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
+        <Link to="/score">
+          <p>Scores</p>
+        </Link>
+      </div>
 
-        <div className="fight-container flex flex-row items-center pb-8 h-full">
-          {" "}
-          <div className="pokecard border rounded-lg p-4 mx-4 mb-8 text-center w-72 h-90">
-            {" "}
+      <div className="fight-container flex flex-row items-center">
+        {" "}
+        <motion.div
+          whileHover={{ scale: 1.04 }}
+          transition={{ type: "spring", duration: 0.2, bounce: true }}
+          className="pokecard"
+        >
+          <div className="pokecard-header">
             <h2 className="pokecard-title">{selectedPokemon.name}</h2>
+            <p className="pokecard-hp text-black font-semibold">
+              HP: {selectedPokemon.stats.hp}
+            </p>
+          </div>
+          <div className="pokecard-img-container">
+            <div className="pokecard-background">
+              <img
+                src={electric}
+                alt="Pokecard background"
+                className="pokecard-background_img"
+              />
+            </div>
             <img
-              className="pokecard-image"
+              className="pokecard-image w-auto h-[70%]"
               src={selectedPokemon.image}
               alt={selectedPokemon.name}
             />
-            <p className="pokecard-stats">HP: {selectedPokemon.stats.hp}</p>
-            <p className="pokecard-stats">
-              Attack: {selectedPokemon.stats.attack}
-            </p>
-            <p className="pokecard-stats">
-              Defense: {selectedPokemon.stats.defense}
-            </p>
-            <p className="pokecard-type">
+            <p className="pokecard-order">{selectedPokemon.order}</p>
+            <div className="pokecard-stats">
+              <span className="pokecard-attack">
+                Attack: {selectedPokemon.stats.attack}
+              </span>
+              <span className="pokecard-defense">
+                Defense: {selectedPokemon.stats.defense}
+              </span>
+            </div>
+          </div>
+
+          <div className="pokecard-body">
+            <p className="pokecard-type text-black font-bold">
               Type:{" "}
               {selectedPokemon.types.map((type) => type.type.name).join(", ")}
             </p>
-            <p className="pokecard-description">
+            <p className="text-black line-clamp-2 px-4 py-6 text-sm">
               {selectedPokemon.description}
             </p>
+            <div className="pokecard-buttons">
+              <Link to={`/home`}>
+                <p className="pokecard-details">BACK</p>
+              </Link>
+
+              <p
+                onClick={() => window.location.reload()}
+                className="pokecard-fgt"
+              >
+                FIGHT AGAIN
+              </p>
+            </div>
           </div>
-          <div className="vs">VS</div>
-          <div className="pokecard">
+        </motion.div>
+        <img src={vs} alt="VS" className="w-[150px] px-4" />
+        <motion.div
+          whileHover={{ scale: 1.04 }}
+          transition={{ type: "spring", duration: 0.2, bounce: true }}
+          className="pokecard"
+        >
+          <div className="pokecard-header">
             <h2 className="pokecard-title">{randomPokemon.name}</h2>
+            <p className="pokecard-hp text-black font-semibold">
+              HP: {randomPokemon.stats.hp}
+            </p>
+          </div>
+          <div className="pokecard-img-container">
+            <div className="pokecard-background">
+              <img
+                src={electric}
+                alt="Pokecard background"
+                className="pokecard-background_img"
+              />
+            </div>
             <img
-              className="pokecard-image"
+              className="pokecard-image w-auto h-[70%]"
               src={randomPokemon.image}
               alt={randomPokemon.name}
             />
-            <p className="pokecard-stats">HP: {randomPokemon.stats.hp}</p>
-            <p className="pokecard-stats">
-              Attack: {randomPokemon.stats.attack}
-            </p>
-            <p className="pokecard-stats">
-              Defense: {randomPokemon.stats.defense}
-            </p>
-            <p className="pokecard-type">
+            <p className="pokecard-order">{randomPokemon.order}</p>
+            <div className="pokecard-stats">
+              <span className="pokecard-attack">
+                Attack: {randomPokemon.stats.attack}
+              </span>
+              <span className="pokecard-defense">
+                Defense: {randomPokemon.stats.defense}
+              </span>
+            </div>
+          </div>
+
+          <div className="pokecard-body">
+            <p className="pokecard-type text-black font-bold">
               Type:{" "}
               {randomPokemon.types.map((type) => type.type.name).join(", ")}
             </p>
-            <p className="pokecard-description">{randomPokemon.description}</p>
+            <p className="text-black px-4 py-6 text-sm">
+              {randomPokemon.description}
+            </p>
           </div>
-          <div className="result">
-            <h2>{winner ? `${winner} wins!` : "It's a tie!"}</h2>
-          </div>
-          <div className="fight-buttons">
-            <Link to="/home">
-              <button className="btn">Back to Home</button>
-            </Link>
-            <button className="btn" onClick={handlePlay}>
-              Fight Now
-            </button>
-          </div>
-        </div>
+        </motion.div>
       </div>
-    </>
+      <div className="fight-button-container text-center mt-4">
+        <button
+          className="fight-button bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={handleCheckWinner}
+          // onClick={() => setShowResult(true)}
+        >
+          FIGHT
+        </button>
+      </div>
+
+      {showResult && (
+        <div className="result text-center text-white px-4 py-2 mt-4">
+          <h2>{winner ? `${winner} wins!` : "It's a tie!"}</h2>
+
+          <button
+            className="save-button bg-green-500 text-white px-4 py-2 rounded mt-4"
+            onClick={saveResult}
+          >
+            Save Result
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 
