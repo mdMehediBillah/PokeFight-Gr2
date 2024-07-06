fa1bd55b756b7ba21518ae56f66e0c5f9b7713a5
mdMehediBillah <mailbillah@gmail.com>
2024-07-06 12:53:22 +0200

    Modal page added

---
+++ Client/src/Components/LoseModal.jsx
@@ -0,0 +1,50 @@
+import "./SuccessModal.css";
+import { motion } from "framer-motion";
+
+const LoseModal = ({ closeModal, selectedPokemon }) => {
+  const userName = localStorage.getItem("userName");
+
+  const handleCloseModal = () => {
+    closeModal(false);
+  };
+
+  return (
+    <div className="modalBg">
+      <motion.div
+        initial={{ scale: 0, opacity: 0, rotateX: 180, x: "-100vw" }}
+        animate={{ scale: 1, opacity: 1, rotateX: 0, x: 0 }}
+        transition={{ type: "spring", duration: 0.2, bounce: 20 }}
+        className="loseModalContainer"
+      >
+        <div className="titleCloseBtn">
+          <button onClick={handleCloseModal}> X </button>
+        </div>
+        <div>
+          <img
+            className="modalImage"
+            src={selectedPokemon.image}
+            alt={selectedPokemon.name}
+          />
+          <h2 className="text-center text-3xl font-semibold mt-24  py-2 text-black">
+            Sorry! {userName}
+          </h2>
+        </div>
+        <div>
+          <p className="text-center mt-2 text-black text-xl">
+            Your Pokemon <strong>{selectedPokemon.name}</strong> lose
+          </p>
+        </div>
+        <div className="flex mt-6 justify-between gap-4 mt-16">
+          <button className=" bg-orange-300 hover:bg-orange-200 text-black font-semibold py-2 rounded w-full">
+            Play again
+          </button>
+          <button className=" bg-cyan-400 hover:bg-cyan-200 text-black font-semibold py-2 rounded w-full">
+            Save
+          </button>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default LoseModal;
--- Client/src/pages/GamePage/Gamepage.jsx
+++ Client/src/pages/GamePage/Gamepage.jsx
@@ -5,13 +5,30 @@ import imgUrl from "../../images/gamePlayBg.jpg";
 import { useEffect, useState } from "react";
 //import { useLocation } from "react-router-dom";
 import axios from "axios";
+import SuccessModal from "../../Components/SuccessModal";
+import LoseModal from "../../Components/LoseModal.jsx";
 
 const Gamepage = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [openLoseModal, setOpenLoseModal] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
   const selectedPokemon = location.state?.selectedPokemon;
   const [randomPokemon, setRandomPokemon] = useState(null);
   const [winner, setWinner] = useState(null);
+
+  const handlePlay = () => {
+    setOpenModal(true);
+    // const playerChoice = Math.floor(Math.random() * 3) + 1;
+    // const randomChoice = Math.floor(Math.random() * 3) + 1;
+    // const result = playerChoice === randomChoice? "It's a tie!" : playerChoice > randomChoice? "Player wins!" : "Computer wins!";
+    // setWinner(result);
+    // setOpenModal(true);
+    // navigate("/result", { state: { selectedPokemon, randomPokemon } });
+  };
+  const handlePlay2 = () => {
+    setOpenLoseModal(true);
+  };
 
   useEffect(() => {
     if (!selectedPokemon) {
@@ -61,7 +78,7 @@ const Gamepage = () => {
 
     fetchRandomPokemon();
   }, [selectedPokemon, navigate]);
-
+  // ===
   useEffect(() => {
     if (selectedPokemon && randomPokemon) {
       const determineWinner = () => {
@@ -96,82 +113,105 @@ const Gamepage = () => {
   }
 
   return (
-    <div
-      className=" homeBg min-h-screen bg-cover bg-center bg-no-repeat w-[100%]"
-      style={{
-        backgroundImage: `url(${imgUrl})`,
-      }}
-    >
-      <h1 className="text-center bg-cyan-50 pt-8 text-4xl font-semibold">
-        {" "}
-        Fight with Pokemon
-      </h1>
-      <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
-        <Link to="/home">
-          <p>Change</p>
-        </Link>
-        <p>Fight</p>
-      </div>
-      <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
-        <Link to="/score">
-          <p>Scores</p>
-        </Link>
-      </div>
+    <>
+      {(openModal && (
+        <SuccessModal
+          closeModal={setOpenModal}
+          selectedPokemon={selectedPokemon}
+        />
+      )) ||
+        (openLoseModal && (
+          <LoseModal
+            closeModal={setOpenLoseModal}
+            selectedPokemon={selectedPokemon}
+          />
+        ))}
 
-      <div className="fight-container flex flex-row items-center pb-8 h-full">
-        {" "}
-        <div className="pokecard border rounded-lg p-4 mx-4 mb-8 text-center w-72 h-90">
+      <div
+        className=" homeBg min-h-screen bg-cover bg-center bg-no-repeat w-[100%]"
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+        }}
+      >
+        <h1 className="text-center bg-cyan-50 pt-8 text-4xl font-semibold">
           {" "}
-          <h2 className="pokecard-title">{selectedPokemon.name}</h2>
-          <img
-            className="pokecard-image"
-            src={selectedPokemon.image}
-            alt={selectedPokemon.name}
-          />
-          <p className="pokecard-stats">HP: {selectedPokemon.stats.hp}</p>
-          <p className="pokecard-stats">
-            Attack: {selectedPokemon.stats.attack}
-          </p>
-          <p className="pokecard-stats">
-            Defense: {selectedPokemon.stats.defense}
-          </p>
-          <p className="pokecard-type">
-            Type:{" "}
-            {selectedPokemon.types.map((type) => type.type.name).join(", ")}
-          </p>
-          <p className="pokecard-description">{selectedPokemon.description}</p>
-        </div>
-        <div className="vs">VS</div>
-        <div className="pokecard">
-          <h2 className="pokecard-title">{randomPokemon.name}</h2>
-          <img
-            className="pokecard-image"
-            src={randomPokemon.image}
-            alt={randomPokemon.name}
-          />
-          <p className="pokecard-stats">HP: {randomPokemon.stats.hp}</p>
-          <p className="pokecard-stats">Attack: {randomPokemon.stats.attack}</p>
-          <p className="pokecard-stats">
-            Defense: {randomPokemon.stats.defense}
-          </p>
-          <p className="pokecard-type">
-            Type: {randomPokemon.types.map((type) => type.type.name).join(", ")}
-          </p>
-          <p className="pokecard-description">{randomPokemon.description}</p>
-        </div>
-        <div className="result">
-          <h2>{winner ? `${winner} wins!` : "It's a tie!"}</h2>
-        </div>
-        <div className="fight-buttons">
+          Fight with Pokemon
+        </h1>
+        <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
           <Link to="/home">
-            <button className="btn">Back to Home</button>
+            <p>Change</p>
           </Link>
-          <button className="btn" onClick={() => window.location.reload()}>
-            Fight Again
-          </button>
+          <p>Fight</p>
+        </div>
+        <div className="flex gap-4 w-72 bg-cyan-50 justify-center mx-auto font-semibold mt-6">
+          <Link to="/score">
+            <p>Scores</p>
+          </Link>
+        </div>
+
+        <div className="fight-container flex flex-row items-center pb-8 h-full">
+          {" "}
+          <div className="pokecard border rounded-lg p-4 mx-4 mb-8 text-center w-72 h-90">
+            {" "}
+            <h2 className="pokecard-title">{selectedPokemon.name}</h2>
+            <img
+              className="pokecard-image"
+              src={selectedPokemon.image}
+              alt={selectedPokemon.name}
+            />
+            <p className="pokecard-stats">HP: {selectedPokemon.stats.hp}</p>
+            <p className="pokecard-stats">
+              Attack: {selectedPokemon.stats.attack}
+            </p>
+            <p className="pokecard-stats">
+              Defense: {selectedPokemon.stats.defense}
+            </p>
+            <p className="pokecard-type">
+              Type:{" "}
+              {selectedPokemon.types.map((type) => type.type.name).join(", ")}
+            </p>
+            <p className="pokecard-description">
+              {selectedPokemon.description}
+            </p>
+          </div>
+          <div className="vs">VS</div>
+          <div className="pokecard">
+            <h2 className="pokecard-title">{randomPokemon.name}</h2>
+            <img
+              className="pokecard-image"
+              src={randomPokemon.image}
+              alt={randomPokemon.name}
+            />
+            <p className="pokecard-stats">HP: {randomPokemon.stats.hp}</p>
+            <p className="pokecard-stats">
+              Attack: {randomPokemon.stats.attack}
+            </p>
+            <p className="pokecard-stats">
+              Defense: {randomPokemon.stats.defense}
+            </p>
+            <p className="pokecard-type">
+              Type:{" "}
+              {randomPokemon.types.map((type) => type.type.name).join(", ")}
+            </p>
+            <p className="pokecard-description">{randomPokemon.description}</p>
+          </div>
+          <div className="result">
+            <h2>{winner ? `${winner} wins!` : "It's a tie!"}</h2>
+          </div>
+          <div className="fight-buttons">
+            <Link to="/home">
+              <button className="btn">Back to Home</button>
+            </Link>
+            <button className="btn" onClick={handlePlay}>
+              Fight Now
+            </button>
+            <button className="btn" onClick={handlePlay2}>
+              Fight Now 2
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
